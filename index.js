7b11aa7bef2aa2edbb0deeb66d9ae9dac0db7c8d
Joacoetruuu <joacoetruuu@gmail.com>
2022-04-16 22:26:32 -0300

    Modo oscuro top

--- index.js
+++ index.js
@@ -3,15 +3,25 @@ var variablescss = getComputedStyle(document.documentElement);
 var variable = String(variablescss.getPropertyValue('--backgroudwhite')).trim();
 var variableletras = String(variablescss.getPropertyValue('--tipewhite')).trim();
 
+function scrolltop(){
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
+
 checkbox.addEventListener("change", function() {
     if(this.checked) {
         console.log("Chequeado")
         document.documentElement.style.setProperty('--backgroudwhite', '#111111');
         document.documentElement.style.setProperty("--tipewhite", "#111111")
         console.log(variable)
+        scrolltop()
+
     }
     else{
         document.documentElement.style.setProperty('--backgroudwhite', '#f3f4f5');
         document.documentElement.style.setProperty('--tipewhite', 'white');
+        scrolltop()
     }
 })
