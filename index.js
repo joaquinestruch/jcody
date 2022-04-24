87fdf7622fbb635234636d3566f3c1677a2ae7fb
Joacoetruuu <joacoetruuu@gmail.com>
2022-04-24 13:30:36 -0300

    Jcody Web full modo oscuro

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ checkbox.addEventListener("change", function() {
         document.documentElement.style.setProperty('--backgroudwhite', '#111111');
         document.documentElement.style.setProperty("--tipewhite", "#111111")
         document.documentElement.style.setProperty("--contrario", "white")
+        document.getElementById("img1").src="./imagenes/plantilla1_negro.png"
+        document.getElementById("img2").src="./imagenes/plantilla2_negro.png"
+        document.getElementById("img3").src="./imagenes/plantilla3_negro.png"
         console.log(variable)
         scrolltop()
 
@@ -24,6 +27,9 @@ checkbox.addEventListener("change", function() {
         document.documentElement.style.setProperty('--backgroudwhite', '#f3f4f5');
         document.documentElement.style.setProperty('--tipewhite', 'white');
         document.documentElement.style.setProperty('--contrario', 'black');
+        document.getElementById("img1").src="./imagenes/plantilla 1.png"
+        document.getElementById("img2").src="./imagenes/plantilla2.png"
+        document.getElementById("img3").src="./imagenes/plantilla 3.png"
         scrolltop()
     }
 })
